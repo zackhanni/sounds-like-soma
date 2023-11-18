d5c762b7b023390de1b70c886c7f1779fdae91f6
Zack Hanni <zack.hannI@gmail.com>
2023-11-18 17:22:39 -0500

    update button colors  and padding

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -9,7 +9,7 @@ interface ButtonProps {
 export default function Button(props: ButtonProps) {
   return (
     <Link href={props.link}>
-      <div className="text-black text-xl font-bold bg-amber-400 hover:bg-amber-500 py-3 px-6 rounded-md duration-200">
+      <div className="text-xl bg-purple-800 hover:bg-purple-950 py-2 px-4 rounded-md duration-200 max-w-fit">
         {props.name}
       </div>
     </Link>
