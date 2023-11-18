706cc4825befee0bef08c21bc54492f263348b72
Zack Hanni <zack.hannI@gmail.com>
2023-11-18 17:25:05 -0500

    add new components and images to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,8 @@ import Contact from "./components/Contact";
 import FillerParagraph from "./components/FillerParagraph";
 import Button from "./components/Button";
 import FillerEveryStepOfTheWay from "./components/FillerEveryStepOfTheWay";
+import FillerLocationMap from "./components/FillerLocationMap";
+import Image from "next/image";
 
 export default function Home() {
   const [open, setOpen] = useState<number | false>(false);
@@ -25,8 +27,23 @@ export default function Home() {
     <div className="bg-[#2E2E2E] flex flex-col justify-center items-center">
       <Hero />
       <Services />
+      <Image
+        src="/best-philadelphia-recording-studios.webp"
+        width={548}
+        height={383}
+        alt="Music Studios In Philadelphia"
+        className=""
+      />
+      <FillerLocationMap />
       <FillerEveryStepOfTheWay />
       <FillerParagraph useData={FillerDataHome} />
+      <Image
+        src="/recording-studios-philadelphia.webp"
+        width={548}
+        height={383}
+        alt="Recording Studios In Philadelphia"
+        className="py-32"
+      />
       <Contact />
       <Accordion useData={AccordionDataHome} />
     </div>
