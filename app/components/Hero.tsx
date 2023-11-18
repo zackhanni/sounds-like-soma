c983bffda9104b308db1ecd080b7360a8e559b08
Zack Hanni <zack.hannI@gmail.com>
2023-11-18 17:24:30 -0500

    new hero component basic layout

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,5 +1,27 @@
-import React from "react";
+import Image from "next/image";
+import CallBox from "./CallBox";
 
 export default function Hero() {
-  return <div>Hero</div>;
+  return (
+    <div className="flex">
+      <div>
+        <h1 className="text-4xl font-bold">
+          Music Recording Studios In Philadelphia
+        </h1>
+        <p className="text-2xl py-8">
+          Philadelphia Recording Studios for Professional Audio Editing and
+          Music Mixing
+        </p>
+        <CallBox />
+      </div>
+      <div>
+        <Image
+          src="/music-studios-philadelphia.webp"
+          width={850}
+          height={633}
+          alt="Philadelphia's Best Recording Studios"
+        />
+      </div>
+    </div>
+  );
 }
