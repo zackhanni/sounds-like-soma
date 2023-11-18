6a59fd809404bd0bb6f5b59b1d81837f1b47fcd8
Zack Hanni <zack.hannI@gmail.com>
2023-11-18 00:59:42 -0500

    make default button

---
+++ app/components/Button.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import React from "react";
+
+interface ButtonProps {
+  name: string;
+  link: string;
+}
+
+export default function Button(props: ButtonProps) {
+  return (
+    <Link href={props.link}>
+      <div className="text-black text-xl font-bold bg-amber-400 hover:bg-amber-500 py-3 px-6 rounded-md m-2 flex">
+        {props.name}
+      </div>
+    </Link>
+  );
+}
