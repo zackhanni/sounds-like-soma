0fcbc4deed7962d2dfa51c9232996d136d8dc193
Zack Hanni <zack.hannI@gmail.com>
2023-11-18 03:35:09 -0500

    create services section and cards

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -1,5 +1,38 @@
+"use client";
+
+import { useInView } from "react-intersection-observer";
 import React from "react";
+import ServicesData from "./ServicesData";
+import Link from "next/link";
+import Button from "./Button";
 
 export default function Services() {
-  return <div>Services</div>;
+  return (
+    <div className="flex flex-wrap items-center justify-center gap-10 py-32 overflow-hidden max-w-[1450px]">
+      {ServicesData.map((service) => {
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        const { ref, inView } = useInView({
+          threshold: 0.15,
+          triggerOnce: true,
+        });
+        return (
+          <div
+            key={service.title}
+            ref={ref}
+            className={`card bg-[#1c1c1c] flex flex-row flex-wrap rounded-lg shadow-2xl p-4 max-w-[600px] min-w-[300px] ${
+              inView ? "show" : "hide"
+            }`}
+          >
+            <Link href={service.link}>
+              <h2 className="text-3xl pb-4 text-[#7115bc] font-bold">
+                {service.title}
+              </h2>
+              <p className="pb-4">{service.description}</p>
+              <Button link={service.link} name="Learn More" />
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
---
+++ app/components/ServicesData.tsx
@@ -0,0 +1,40 @@
+const ServicesData = [
+  {
+    title: "Recording Studios",
+    description:
+      "World-class recording and professional mixing studio. Get your next music project completed by our Philly team with state-of-the-art audio equipment",
+    link: "/services/recording-studios",
+  },
+  {
+    title: "Mixing And Mastering",
+    description:
+      "We use our diverse knowledge of multiple genres and decades of experience of to transform your recorded tracks into a finished product ready for commercial release",
+    link: "/services/mixing-mastering",
+  },
+  {
+    title: "Podcast Production",
+    description:
+      "Philly’s top podcast production studio invites you to work with our team of audio editing experts. Equipped with crystal-clear SM7B broadcast dynamic microphones",
+    link: "/services/podcast-recording-studios",
+  },
+  {
+    title: "Voice Over Recording",
+    description:
+      "Our expert Voice Over production team invites you to record in our soundproof vocal booths. Equipped with crystal-clear Neumann Condenser microphones",
+    link: "/services/voiceover-recording-studios",
+  },
+  {
+    title: "Rentable Music Studio Space",
+    description:
+      "Rent a Philadelphia music studio. Utilize HD audio equipment and condenser microphones to record yourself in a private room. Affordable prices",
+    link: "/services/rent-studio-space",
+  },
+  {
+    title: "Band Rehearsals",
+    description:
+      "Philadelphia’s Best Hourly Band Rehearsal Studios. Equipped With Drums, Amps, Keyboards, Vocal Mics And More. Studio D - Live Room - Music Practice space",
+    link: "/services/band-rehearsal-studio",
+  },
+];
+
+export default ServicesData;
