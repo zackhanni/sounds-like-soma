92845273192d2c386b7c273c9548bd156048b2ff
Zack Hanni <zack.hannI@gmail.com>
2023-11-18 03:52:06 -0500

    center all homepage parts

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,11 @@ import Accordion from "./components/Accordion";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
 import AccordionDataHome from "./components/AccordionDataHome";
+import FillerDataHome from "./components/FillerDataHome";
+import Contact from "./components/Contact";
+import FillerParagraph from "./components/FillerParagraph";
+import Button from "./components/Button";
+import FillerEveryStepOfTheWay from "./components/FillerEveryStepOfTheWay";
 
 export default function Home() {
   const [open, setOpen] = useState<number | false>(false);
@@ -17,9 +22,12 @@ export default function Home() {
   };
 
   return (
-    <div className="bg-[#2E2E2E] w-full h-full">
+    <div className="bg-[#2E2E2E] flex flex-col justify-center items-center">
       <Hero />
       <Services />
+      <FillerEveryStepOfTheWay />
+      <FillerParagraph useData={FillerDataHome} />
+      <Contact />
       <Accordion useData={AccordionDataHome} />
     </div>
   );
