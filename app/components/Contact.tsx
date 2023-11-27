e715e6cb5ea0ed9bd744a07ff04201e7d1e0fb27
Zack Hanni <zack.hannI@gmail.com>
2023-11-27 17:05:22 -0500

    contact center text

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 
 export default function Contact() {
   return (
-    <div className="grid place-items-center bg-[#1c1c1c] py-32 w-full ">
+    <div className="grid place-items-center text-center bg-[#1c1c1c] py-32 w-full">
       <h2>Contact us</h2>
       <p className="text-6xl py-4">Want to learn more?</p>
       <p className="pb-8">
