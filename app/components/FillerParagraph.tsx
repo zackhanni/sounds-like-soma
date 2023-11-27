b06367ed06f67d89096130858e4c501d40914110
Zack Hanni <zack.hannI@gmail.com>
2023-11-27 17:07:09 -0500

    change width of filler paragraphs

--- app/components/FillerParagraph.tsx
+++ app/components/FillerParagraph.tsx
@@ -10,10 +10,10 @@ interface FillerParagraphProps {
 
 export default function FillerParagraph(props: FillerParagraphProps) {
   return (
-    <div className="max-w-[1450px]">
+    <div className="max-w-[90%] sm:max-w-[75%] 2xl:max-w-[50%]">
       {props.useData.map((filler) => {
         return (
-          <div key={filler.title} className="p-4">
+          <div key={filler.title} className="py-4">
             <h3 className="text-2xl">{filler.title}</h3>
             <p>{filler.paragraph}</p>
           </div>
