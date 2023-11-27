a80c7dae8c0f2f134b60530cbfce4331299e8634
Zack Hanni <zack.hannI@gmail.com>
2023-11-27 17:04:43 -0500

    make button text ivory

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -9,7 +9,7 @@ interface ButtonProps {
 export default function Button(props: ButtonProps) {
   return (
     <Link href={props.link}>
-      <div className="text-xl bg-purple-800 hover:bg-purple-950 py-2 px-4 rounded-md duration-200 max-w-fit">
+      <div className="text-xl text-[#fffff0] bg-purple-800 hover:bg-purple-950 py-2 px-4 rounded-md duration-200 max-w-fit">
         {props.name}
       </div>
     </Link>
