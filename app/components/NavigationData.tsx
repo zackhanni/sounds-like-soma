0f11e25ee6b64f7460de6ac87c28602c48ae4f80
Zack Hanni <zack.hannI@gmail.com>
2023-11-27 17:05:56 -0500

    Create NavigationData.tsx

---
+++ app/components/NavigationData.tsx
@@ -0,0 +1,28 @@
+const NavigationData = [
+  {
+    name: "Home",
+    link: "/",
+  },
+  {
+    name: "Services",
+    link: "/services",
+  },
+  {
+    name: "Samples",
+    link: "/samples",
+  },
+  {
+    name: "Blog",
+    link: "/blog",
+  },
+  {
+    name: "Reviews",
+    link: "/reviews",
+  },
+  {
+    name: "Contact",
+    link: "/contact",
+  },
+];
+
+export default NavigationData;
